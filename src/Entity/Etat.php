39d8f81bb62900a4d2139d11e4e764cab7df4afd
Alexandre <alexgalaxy67@gmail.com>
2022-07-18 13:45:43 +0200

    //

--- src/Entity/Etat.php
+++ src/Entity/Etat.php
@@ -7,6 +7,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+/*
+ */
+
+
 /**
  * @ORM\Entity(repositoryClass=EtatRepository::class)
  */
