83281cbc634b9526ed798349068bedf2022e2a3c
Reaper95 <gouget.a@gmail.com>
2022-07-18 15:46:11 +0200

    debug login and logout

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Config\SecurityConfig;
 
 class AuthController extends AbstractController
 {
@@ -18,10 +22,13 @@ class AuthController extends AbstractController
             return $this->redirectToRoute('main_home');
         }
 
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
+
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
@@ -31,6 +38,7 @@ class AuthController extends AbstractController
      */
     public function logout(): void
     {
+
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
