37ed2dfe0f6337338817296f53e676ccbc9cd872
Reaper95 <gouget.a@gmail.com>
2022-07-26 15:36:14 +0200

    Refactorisation fonctionnalité de recherche

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,46 +4,42 @@ namespace App\Controller;
 
 use App\Entity\Sortie;
 use App\Entity\User;
+use App\Form\FormulaireRecherche;
 use App\Repository\EtatRepository;
 use App\Repository\SortieRepository;
 use App\Repository\UserRepository;
 use App\Services\EtatSortie;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="main_home")
      */
-    public function home(SortieRepository $sortieRepo, EtatSortie $etatSortie): Response
+    public function home(Request $request,SortieRepository $sortieRepo, EtatSortie $etatSortie): Response
     {
         $etatSortie->checkAndUpdateEtatAll();
         $sorties = $sortieRepo->findSortiesOuverte(16);
 
-        $etatFini = $etatRepository->findOneBy(array('libelle'=> 'Fini'));
-        $etatArchive = $etatRepository->findOneBy(array('libelle'=> 'Archive'));
+        $formulaireRecherche=$this->createForm(FormulaireRecherche::class);
+        $formulaireRecherche->handleRequest($request);
 
-        foreach ($sorties as $sortie){
-
-            if($sortie->getDateHeureDebut() >= new \DateTime('now')){
-                $sortie->setEtat($etatFini);
-                $em->flush();
-            }
-            if($sortie->getDateHeureDebut() >= new \DateTime('now'."30 days")){
-                $sortie->setEtat($etatArchive);
-                $em->flush();
-            }
-
-
+        if($formulaireRecherche->isSubmitted()&&$formulaireRecherche->isValid()){
+            return $this->redirectToRoute('app_event_research', [
+                'request' => $request
+            ], 307);
         }
 
 
-        return $this->render('main/index.html.twig', [
+          return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
-            'listSortie' => $sorties
+            'listSortie' => $sorties,
+              'searchForm'=>$formulaireRecherche->createView()
         ]);
     }
 
