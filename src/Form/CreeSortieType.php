c9d8c8a9bd9b00a42ab346098ce07ef29a177ce8
alexgitENI <alexgalaxy67@gmail.com>
2022-07-27 15:35:30 +0200

    Modif import

--- src/Form/CreeSortieType.php
+++ src/Form/CreeSortieType.php
@@ -4,17 +4,12 @@ namespace App\Form;
 
 use App\Entity\Campus;
 use App\Entity\Categorie;
-use App\Entity\Etat;
 use App\Entity\Lieu;
 use App\Entity\Sortie;
-use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
